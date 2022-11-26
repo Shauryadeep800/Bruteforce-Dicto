6439e33b579befb9b7054584a2b01c0d20e38e6f
Shauryadeep800 <81960829+Shauryadeep800@users.noreply.github.com>
2022-11-26 19:03:10 +0530

    Create dicto.py

---
+++ dicto.py
@@ -0,0 +1,114 @@
+import random
+name = input("Name of victim: ")
+sname = input ("Surname of victim: ")
+num = input("NumbersA for victim: ")
+num2 = input("NumbersB for victim: ")
+frnd = input("Friend of victim: ")
+spl = input ("special character for victim: ")
+word1 = input("realted word1: ")
+word2 = input("realted word2: ")
+word3 = input("realted word3: ")
+
+#SCRIPT 
+
+print (name+sname)
+print (name+num[0]+num[1]+num[2]+spl)
+print (name+num[0]+num[1]+num[3]+num[4]+spl)
+print (name+num[0]+num[1]+num[2]+num[3]num[4]+num[5]+num[6])
+print (num[0]+num[1]+num[2]+num[3]num[4]+num[5]+num[6])
+print (num[0]+num[1]+num[2]+num[3]num[4]+num[5]+num[6]+num[7]+num[8])
+print (name+frnd+"@")
+print (name+frnd+"#")
+print (name+"@"+frnd)
+print (frnd+"123@")
+print (frnd+"123#")
+
+print (frnd+num[0]+num[1]+num[2]+spl)
+print (frnd+num[0]+num[1]+num[3]+num[4]+spl)
+print (frnd+num[0]+num[1]+num[2]+num[3]num[4]+num[5]+num[6])
+print (num[0]+num[1]+num[2]+num[3]num[4]+num[5]+num[6])
+print (num[0]+num[1]+num[2]+num[3]num[4]+num[5]+num[6]+num[7]+num[8])
+
+print (name+sname)
+print (name+num2[0]+num2[1]+num2[2]+spl)
+print (name+num2[0]+num2[1]+num2[3]+num2[4]+spl)
+print (name+num2[0]+num2[1]+num2[2]+num2[3]num2[4]+num2[5]+num2[6])
+print (num2[0]+num2[1]+num2[2]+num2[3]num2[4]+num2[5]+num2[6])
+print (num2[0]+num2[1]+num2[2]+num2[3]num2[4]+num2[5]+num2[6]+num2[7]+num2[8])
+print (name+frnd+"@")
+print (name+frnd+"#")
+print (name+"@"+frnd)
+print (frnd+"123@")
+print (frnd+"123#")
+
+print (frnd+num2[0]+num2[1]+num2[2]+spl)
+print (frnd+num2[0]+num2[1]+num2[3]+num2[4]+spl)
+print (frnd+num2[0]+num2[1]+num2[2]+num2[3]num2[4]+num2[5]+num2[6])
+print (num2[0]+num2[1]+num2[2]+num2[3]num2[4]+num2[5]+num2[6])
+print (num2[0]+num2[1]+num2[2]+num2[3]num2[4]+num2[5]+num2[6]+num2[7]+num2[8])
+
+
+#WORD1
+
+print (name+sname)
+print (name+word1[0]+word1[1]+word1[2]+spl)
+print (name+word1[0]+word1[1]+word1[3]+word1[4]+spl)
+print (name+word1[0]+word1[1]+word1[2]+word1[3]word1[4]+word1[5]+word1[6])
+print (word1[0]+word1[1]+word1[2]+word1[3]word1[4]+word1[5]+word1[6])
+print (word1[0]+word1[1]+word1[2]+word1[3]word1[4]+word1[5]+word1[6]+word1[7]+word1[8])
+print (name+frnd+"@")
+print (name+frnd+"#")
+print (name+"@"+frnd)
+print (frnd+"123@")
+print (frnd+"123#")
+
+print (frnd+word1[0]+word1[1]+word1[2]+spl)
+print (frnd+word1[0]+word1[1]+word1[3]+word1[4]+spl)
+print (frnd+word1[0]+word1[1]+word1[2]+word1[3]word1[4]+word1[5]+word1[6])
+print (word1[0]+word1[1]+word1[2]+word1[3]word1[4]+word1[5]+word1[6])
+print (word1[0]+word1[1]+word1[2]+word1[3]word1[4]+word1[5]+word1[6]+word1[7]+word1[8])
+
+
+#WORD2
+
+print (name+sname)
+print (name+word2[0]+word2[1]+word2[2]+spl)
+print (name+word2[0]+word2[1]+word2[3]+word2[4]+spl)
+print (name+word2[0]+word2[1]+word2[2]+word2[3]word2[4]+word2[5]+word2[6])
+print (word2[0]+word2[1]+word2[2]+word2[3]word2[4]+word2[5]+word2[6])
+print (word2[0]+word2[1]+word2[2]+word2[3]word2[4]+word2[5]+word2[6]+word2[7]+word2[8])
+print (name+frnd+"@")
+print (name+frnd+"#")
+print (name+"@"+frnd)
+print (frnd+"123@")
+print (frnd+"123#")
+
+print (frnd+word2[0]+word2[1]+word2[2]+spl)
+print (frnd+word2[0]+word2[1]+word2[3]+word2[4]+spl)
+print (frnd+word2[0]+word2[1]+word2[2]+word2[3]word2[4]+word2[5]+word2[6])
+print (word2[0]+word2[1]+word2[2]+word2[3]word2[4]+word2[5]+word2[6])
+print (word2[0]+word2[1]+word2[2]+word2[3]word2[4]+word2[5]+word2[6]+word2[7]+word2[8])
+
+
+#WORD3
+print (name+sname)
+print (name+word3[0]+word3[1]+word3[2]+spl)
+print (name+word3[0]+word3[1]+word3[3]+word3[4]+spl)
+print (name+word3[0]+word3[1]+word3[2]+word3[3]word3[4]+word3[5]+word3[6])
+print (word3[0]+word3[1]+word3[2]+word3[3]word3[4]+word3[5]+word3[6])
+print (word3[0]+word3[1]+word3[2]+word3[3]word3[4]+word3[5]+word3[6]+word3[7]+word3[8])
+print (name+frnd+"@")
+print (name+frnd+"#")
+print (name+"@"+frnd)
+print (frnd+"123@")
+print (frnd+"123#")
+
+print (frnd+word3[0]+word3[1]+word3[2]+spl)
+print (frnd+word3[0]+word3[1]+word3[3]+word3[4]+spl)
+print (frnd+word3[0]+word3[1]+word3[2]+word3[3]word3[4]+word3[5]+word3[6])
+print (word3[0]+word3[1]+word3[2]+word3[3]word3[4]+word3[5]+word3[6])
+print (word3[0]+word3[1]+word3[2]+word3[3]word3[4]+word3[5]+word3[6]+word3[7]+word3[8])
+
+
+
+
